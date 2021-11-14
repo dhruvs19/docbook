cac32c86968306645c0146bb5b70b34eeb10f356
Subin <skuruvilla73@gmail.com>
2021-11-13 20:20:34 -0500

    patient model 1

--- docbook/patients/models.py
+++ docbook/patients/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.base import Model
+from authuser import *
+
 
 # Create your models here.
+class Patients(models.Model):
+    FirstName  = models.CharField(max_length = 65)
+    LastName  = models.CharField(max_length = 65)
+    Address = models.CharField(max_length = 200)
+    PhoneNumber = models.IntegerField()
+    DOB = models.DateField()
+    BloodGroup = models.CharField(max_length = 5)
+    RegistrationNumber = models.ForeignKey()
