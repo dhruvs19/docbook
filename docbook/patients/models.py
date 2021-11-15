5c60cdc787b55382665eba64edc7fae66f505387
Subin <skuruvilla73@gmail.com>
2021-11-14 23:13:31 -0500

    test

--- docbook/patients/models.py
+++ docbook/patients/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 from django.db.models.base import Model
-from authuser import *
+from django.db.models.deletion import CASCADE
 
 
 # Create your models here.
+
+class Users(models.Model):
+    UserID      = models.IntegerField( primary_key = True)
+    UserName    = models.CharField(max_length = 65)
+    Email       = models.EmailField()
+    Password    = models.CharField(max_length = 65)
+    UserType    = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return "{0} {1} {2} {3} {4} {5}".format(
+            self,self.UserID, self.UserName, self.Email, self.Password, self.UserType)
+
+
 class Patients(models.Model):
-    FirstName  = models.CharField(max_length = 65)
-    LastName  = models.CharField(max_length = 65)
-    Address = models.CharField(max_length = 200)
+    PatientId   = models.IntegerField(primary_key = True)
+    UserID      = models.ForeignKey(Users, on_delete = CASCADE)
+    FirstName   = models.CharField(max_length = 65)
+    LastName    = models.CharField(max_length = 65)
+    Address     = models.CharField(max_length = 200)
     PhoneNumber = models.IntegerField()
-    DOB = models.DateField()
-    BloodGroup = models.CharField(max_length = 5)
-    RegistrationNumber = models.ForeignKey()
+    DOB         = models.DateField()
+    BloodGroup  = models.CharField(max_length = 5)
+
+    def __str__(self):
+        return "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} ".format(
+            self, self.PatientId, self.UserID, self.FirstName, self.LastName, self.Address, self.PhoneNumber, self.DOB, self.BloodGroup)
--- docbook/patients/views.py
+++ docbook/patients/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from docbook import patients
+
+from docbook.patients.form import PatientFormset
+from docbook.patients.models import Patients
 
 # Create your views here.
 def PatientProfile(request):
     return render(request, 'patients/profile.html')
+
+
+def savePatient(request):
+    form = PatientsForm()
+    book_formset = PatientFormset(instance = Patients())
+
+    if request.POST:
+        form = PatientsForm(request.POST)
+        if form.is_valid():
+            patients = form.save()
+            book_formset = PatientFormset(request.POST, instance = Patients)
+            if book_formset.is_valid():
+                book_formset.save()
+            return redirect('/index/')
+
+    return render_to_response('addbook.html',{
+        'form': form, 'formset': book_formset
+    },context_instance=RequestContext(request))  
