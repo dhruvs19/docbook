bcb10ee35780ba1c2fd36c4a56f8cc6f71ffa717
Subin <skuruvilla73@gmail.com>
2021-11-16 11:13:23 -0500

    model form

--- docbook/patients/models.py
+++ docbook/patients/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 # Create your models here.
 
@@ -11,8 +12,13 @@ class Patients(models.Model):
     LastName    = models.CharField(max_length = 65)
     Address     = models.CharField(max_length = 200)
     PhoneNumber = models.CharField(max_length = 200)
-    DOB         = models.CharField(max_length = 200)
+    DOB         = models.DateField()
     BloodGroup  = models.CharField(max_length = 5)
 
     def __str__(self):
         return self.UserID.username
+
+class PatientsForm(ModelForm):
+    class Meta:
+        model = Patients
+        fields = ['FirstName', 'LastName', 'Address', 'PhoneNumber', 'DOB', 'BloodGroup']
