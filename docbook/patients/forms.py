09d77b03159f684158e5a7b69051c08a57f05907
Subin <skuruvilla73@gmail.com>
2021-11-30 14:58:31 -0500

    embedded

--- docbook/patients/forms.py
+++ docbook/patients/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import TextInput
 from django.contrib.auth.models import User
 from django.forms import ModelForm, widgets, DateField
-from .models import Patients
+from .models import Diagnosis, Patients
 
 
 class PatientsForm(ModelForm):
@@ -43,6 +43,14 @@ class PatientsForm(ModelForm):
 
             if visible.field.widget.input_type == "select":
                 visible.field.widget.attrs['class'] = "form-select form-control floating"
-    
 
-        
+class MedicalHistoryForm(ModelForm):
+    TestTypes = (
+        ('','Select')
+        ('X-Ray','X-Ray'),
+        ('Complete Blood Count','Complete blood count'),
+        ('Vitamin D Test', 'Vitamin D Test'),
+        ('PULS (Protein Unstable Lesion Signature Test) Cardiac Test' ,'PULS (Protein Unstable Lesion Signature Test) Cardiac Test'),
+        ('ABPM','ABPM')
+    )
+    DiagnosisType = forms.ChoiceField(required=True, choices = GROUPS)
