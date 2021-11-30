f1609093a1f7a970eee0afce3d82017d108f01e1
simranjeet kaur <simran31kaur@gmail.com>
2021-11-30 18:46:04 -0500

    update

--- docbook/doctor/views.py
+++ docbook/doctor/views.py
@@ -11,86 +11,105 @@ from appointments.models import *
 
 
 class HomeView(ListView):
-	model = DocProfile
-	template_name = 'doctor/doctor-dashboard.html'
+    model = DocProfile
+    template_name = 'doctor/doctor-dashboard.html'
 
-	def get_context_data(self, *args, **kwargs):
-		if DocProfile.objects.filter(UserID = self.request.user).exists():
-			doc = DocProfile.objects.get(UserID = self.request.user)
-			app = Appointments.objects.filter(DoctorUser=self.request.user.id,Status="Pending")
-			context = super(HomeView, self).get_context_data(*args, **kwargs)
-			context["doc"] = doc
-			context["app"] = app
-			return context
-		else:
-			messages.error(self.request, "Contact with Admin for registration...")
-    
+    def get_context_data(self, *args, **kwargs):
+        if DocProfile.objects.filter(UserID=self.request.user).exists():
+            doc = DocProfile.objects.get(UserID=self.request.user)
+            app = Appointments.objects.filter(
+                DoctorUser=self.request.user.id, Status="Pending")
+            context = super(HomeView, self).get_context_data(*args, **kwargs)
+            context["doc"] = doc
+            context["app"] = app
+            return context
+        else:
+            messages.error(
+                self.request, "Contact with Admin for registration...")
+
+
 class UpdateDoctorView(UpdateView):
-	model = DocProfile
-	form_class = DocProfileForm
-	template_name = 'doctor/profile-setting.html'
+    model = DocProfile
+    form_class = DocProfileForm
+    template_name = 'doctor/profile-setting.html'
 
-	def get_context_data(self, *args, **kwargs):
-		if DocProfile.objects.filter(UserID = self.request.user).exists():
-			doc = DocProfile.objects.get(UserID = self.request.user)
-			context = super(UpdateDoctorView, self).get_context_data(*args, **kwargs)
-			context["doc"] = doc
-			return context
-		else:
-			messages.error(self.request, "Contact with Admin for registration...")
+    def get_context_data(self, *args, **kwargs):
+        if DocProfile.objects.filter(UserID=self.request.user).exists():
+            doc = DocProfile.objects.get(UserID=self.request.user)
+            context = super(UpdateDoctorView, self).get_context_data(
+                *args, **kwargs)
+            context["doc"] = doc
+            return context
+        else:
+            messages.error(
+                self.request, "Contact with Admin for registration...")
+
 
 class ProfileView(CreateView):
-	model = DocProfile
-	form_class = DocProfileForm
-	template_name = 'doctor/view-profile.html'
-	
-	def get_context_data(self, *args, **kwargs):
-		if DocProfile.objects.filter(UserID = self.request.user).exists():
-			id = self.request.user.id
-			update = "/doctor/update/"+str(id)
-			user = DocProfile.objects.get(UserID = self.request.user)
-			context = super(ProfileView, self).get_context_data(*args, **kwargs)
-			context["doc"] = user
-			context["update"] = update
-			return context
-		else:
-			messages.error(self.request, "Contact with Admin for registration...")
+    model = DocProfile
+    form_class = DocProfileForm
+    template_name = 'doctor/view-profile.html'
 
-def acceptView(request,pk):
-	Appointments.objects.filter(AppointmentID=pk,Status="Pending").update(Status="Accepted")
-	return redirect('/doctor/')
+    def get_context_data(self, *args, **kwargs):
+        if DocProfile.objects.filter(UserID=self.request.user).exists():
+            id = self.request.user.id
+            update = "/doctor/update/"+str(id)
+            user = DocProfile.objects.get(UserID=self.request.user)
+            context = super(ProfileView, self).get_context_data(
+                *args, **kwargs)
+            context["doc"] = user
+            context["update"] = update
+            return context
+        else:
+            messages.error(
+                self.request, "Contact with Admin for registration...")
 
-def deleteView(request,pk):
-	Appointments.objects.filter(AppointmentID=pk,Status="Pending").update(Status="Rejected")
-	return redirect('/doctor/')
+
+def acceptView(request, pk):
+    Appointments.objects.filter(
+        AppointmentID=pk, Status="Pending").update(Status="Accepted")
+    return redirect('/doctor/')
+
+
+def deleteView(request, pk):
+    Appointments.objects.filter(
+        AppointmentID=pk, Status="Pending").update(Status="Rejected")
+    return redirect('/doctor/')
+
 
 class PatientListView(ListView):
-	model = DocProfile
-	template_name = 'doctor/patient-list.html'
+    model = DocProfile
+    template_name = 'doctor/patient-list.html'
 
-	def get_context_data(self, *args, **kwargs):
-		if DocProfile.objects.filter(UserID = self.request.user).exists():
-			user = DocProfile.objects.get(UserID = self.request.user)
-			app = Appointments.objects.filter(DoctorUser=self.request.user.id,Status="Accepted")
-			context = super(PatientListView, self).get_context_data(*args, **kwargs)
-			context["doc"] = user
-			context["patients"] = app
-			return context
-		else:
-			messages.error(self.request, "Contact with Admin for registration...")
-	
+    def get_context_data(self, *args, **kwargs):
+        if DocProfile.objects.filter(UserID=self.request.user).exists():
+            user = DocProfile.objects.get(UserID=self.request.user)
+            app = Appointments.objects.filter(
+                DoctorUser=self.request.user.id, Status="Accepted")
+            context = super(PatientListView, self).get_context_data(
+                *args, **kwargs)
+            context["doc"] = user
+            context["patients"] = app
+            return context
+        else:
+            messages.error(
+                self.request, "Contact with Admin for registration...")
+
+
 class PatientRejListView(ListView):
-	model = DocProfile
-	template_name = 'doctor/patient-list.html'
+    model = DocProfile
+    template_name = 'doctor/patient-list.html'
 
-	def get_context_data(self, *args, **kwargs):
-		if DocProfile.objects.filter(UserID = self.request.user).exists():
-			user = DocProfile.objects.get(UserID = self.request.user)
-			app = Appointments.objects.filter(DoctorUser=self.request.user.id,Status="Rejected")
-			context = super(PatientRejListView, self).get_context_data(*args, **kwargs)
-			context["doc"] = user
-			context["patients"] = app
-			return context
-		else:
-			messages.error(self.request, "Contact with Admin for registration...")
-
+    def get_context_data(self, *args, **kwargs):
+        if DocProfile.objects.filter(UserID=self.request.user).exists():
+            user = DocProfile.objects.get(UserID=self.request.user)
+            app = Appointments.objects.filter(
+                DoctorUser=self.request.user.id, Status="Rejected")
+            context = super(PatientRejListView, self).get_context_data(
+                *args, **kwargs)
+            context["doc"] = user
+            context["patients"] = app
+            return context
+        else:
+            messages.error(
+                self.request, "Contact with Admin for registration...")
