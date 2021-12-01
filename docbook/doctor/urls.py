6df17ccac585eb92e160b8372699a8bcb10a97ec
Dhruv Sharma <dhruvsharma1904@gmail.com>
2021-12-01 17:44:25 -0500

    location and specialization models on doctor

--- docbook/doctor/urls.py
+++ docbook/doctor/urls.py
@@ -6,8 +6,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
 app_name = 'doctor'
 
 urlpatterns = [
@@ -19,9 +17,3 @@ urlpatterns = [
     path('update/<int:pk>', UpdateDoctorView.as_view(),name = "doctor-register"),
     path('delete/<int:pk>', views.deleteView, name = "delete-view"),
 ]
-
-
-
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
